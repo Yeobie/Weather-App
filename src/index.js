bf1d42945fe30387446ee92f57eb1d4fe46861e3
Yeobie <konoha.shoujo@gmail.com>
2021-12-05 22:28:30 +0100

    Moved whole project to the folder

---
+++ src/index.js
@@ -0,0 +1,89 @@
+let now = new Date();
+
+let date = document.querySelector("#current-date");
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[now.getDay()];
+
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+date.innerHTML = `${day} ${hours}:${minutes}`;
+
+function getWeather(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let degrees = document.querySelector("#degrees-value");
+  degrees.innerHTML = `${temperature}`;
+  let description = response.data.weather[0].description;
+  let weather = document.querySelector("#weather-description");
+  weather.innerHTML = `${description}`;
+  let wind = Math.round(response.data.wind.speed);
+  let windspeed = document.querySelector("#wind");
+  windspeed.innerHTML = `Wind: ${wind} km/h`;
+  let humidity = response.data.main.humidity;
+  let humidityvalue = document.querySelector("#humidity");
+  humidityvalue.innerHTML = `Humidity: ${humidity}%`;
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let searchedCity = document.querySelector("#search-city-input");
+  let city = document.querySelector("#city-name");
+  city.innerHTML = `${searchedCity.value}`;
+  let apiKey = "c7dca84c0fd098022472de54bb75f43a";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity.value}&units=metric&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(getWeather);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchCity);
+
+function getCurrent(response) {
+  let city = response.data.name;
+  let h1 = document.querySelector("#city-name");
+  h1.innerHTML = `${city}`;
+  let temperature = Math.round(response.data.main.temp);
+  let degrees = document.querySelector("#degrees-value");
+  degrees.innerHTML = `${temperature}`;
+  let description = response.data.weather[0].description;
+  let weather = document.querySelector("#weather-description");
+  weather.innerHTML = `${description}`;
+  let wind = Math.round(response.data.wind.speed);
+  let windspeed = document.querySelector("#wind");
+  windspeed.innerHTML = `Wind: ${wind} km/h`;
+  let humidity = response.data.main.humidity;
+  let humidityvalue = document.querySelector("#humidity");
+  humidityvalue.innerHTML = `Humidity: ${humidity}%`;
+}
+
+function showCurrent(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "c7dca84c0fd098022472de54bb75f43a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(getCurrent);
+}
+
+function showCurrentCity() {
+  navigator.geolocation.getCurrentPosition(showCurrent);
+}
+
+let current = document.querySelector("#current-city");
+current.addEventListener("click", showCurrentCity);
