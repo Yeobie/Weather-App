50632e40c87395735fe1c44d47d137a8a8b7fae9
Yeobie <konoha.shoujo@gmail.com>
2021-12-19 22:18:03 +0100

    Changed my mind: hid "Current" button in HTML, and added Cº/Fº conversion also in the forecast

--- src/index.js
+++ src/index.js
@@ -81,6 +81,12 @@ function getForecast(coordinates) {
   axios.get(apiUrl).then(displayForecast);
 }
 
+function getImperialForecast(coordinates) {
+  let apiKey = "c7dca84c0fd098022472de54bb75f43a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function getWeather(response) {
   celsiusTemperature = response.data.main.temp;
   let temperature = Math.round(celsiusTemperature);
@@ -118,10 +124,29 @@ function searchCity(event) {
   axios.get(apiUrl).then(getWeather);
 }
 
+function getWeatherImperial(response) {
+  getImperialForecast(response.data.coord);
+}
+
+function searchCityImperial(event) {
+  event.preventDefault();
+  let searchedCity = document.querySelector("#search-city-input");
+  let city = document.querySelector("#city-name");
+  city.innerHTML = `${searchedCity.value}`;
+  let apiKey = "c7dca84c0fd098022472de54bb75f43a";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchedCity.value}&units=imperial&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(getWeatherImperial);
+}
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", searchCity);
 let search = document.querySelector("#search-button");
 search.addEventListener("click", searchCity);
+
+//the following part was originally needed for the search of the current city.
+//leaving it here to always have a reference of the code I wrote
 
 function getCurrent(response) {
   let city = response.data.name;
@@ -163,11 +188,10 @@ function showCurrentCity() {
   navigator.geolocation.getCurrentPosition(showCurrent);
 }
 
-let current = document.querySelector("#current-city");
-current.addEventListener("click", showCurrentCity);
+// let current = document.querySelector("#current-city");
+// current.addEventListener("click", showCurrentCity);
 
-//the following functions were originally needed for the Cº/Fº conversion.
-//leaving them here to always have a reference of the code I wrote
+//end of the "current city" part
 
 function convertFahrenheit(event) {
   event.preventDefault();
@@ -196,6 +220,8 @@ let celsiusTemperature = null;
 let windMetric = null;
 let fahrenheitLink = document.querySelector("#fahrenheit-degrees");
 fahrenheitLink.addEventListener("click", convertFahrenheit);
+fahrenheitLink.addEventListener("click", searchCityImperial);
 
 let celsiusLink = document.querySelector("#celsius-degrees");
 celsiusLink.addEventListener("click", convertCelsius);
+celsiusLink.addEventListener("click", searchCity);
